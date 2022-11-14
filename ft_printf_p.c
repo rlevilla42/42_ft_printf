8534b94861aa4d6b551bb2d1981d57a31f0ced78
rlevilla42 <rlevilla42@gmail.com>
2022-11-14 23:24:26 +0100

    4 test

--- ft_printf_p.c
+++ ft_printf_p.c
@@ -6,7 +6,7 @@
 /*   By: rlevilla <rlevilla@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 00:31:54 by rlevilla          #+#    #+#             */
-/*   Updated: 2022/11/14 23:21:54 by rlevilla         ###   ########.fr       */
+/*   Updated: 2022/11/14 23:23:44 by rlevilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ int	ft_printf_p(unsigned long long int n, char *base)
 	return (ft_itoa_base(n, base));
 
 }
-/*
+
 int main(void)
 {
 	//unsigned int	n = 4294967295;
@@ -120,5 +120,4 @@ int main(void)
 	printf("%d\n", ft_printf_p((uintptr_t)ptrn2, "0123456789abcdef"));
 	printf("%d\n", ft_printf_p((unsigned long)ptrn2, "0123456789abcdef"));
 	printf("%d\n", ft_printf_p((long)ptrn2, "0123456789abcdef"));
-test1;
-}*/
+}
